6c5a846aa0f06cc966b6c102f5d405f33ab13744
chatti_nader <chatti.tkd@gmail.com>
2019-01-05 05:26:13 +0100

    Ajout du fichier About.vue (à propos)

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">A propos</h1>
+    <p><b>Gestionnaire de restaurants</b></p>
+    <p>Ce site contient une liste de restaurants présentés sous forme d'un tableau sur plusieurs pages. Vous pouvez visualiser les informations
+      de chaque restaurant (nom, type de cuisine, classe et adresse).</p>
+    <p>Vouv avez la possibilité de soit modifier les informations d'un restaurant existant, de supprimer ou d'ajouter un restaurant.</p>
+    <p>Pour rechercher un restaurant dans le tableau il suffit de rentrer le nom de ce dernier dans la barre de recherhce.</p>
+    <hr>
+    <h5>Ce Site est une application <b>VueJS</b> développée en <b>Mode CLI.</b></h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
