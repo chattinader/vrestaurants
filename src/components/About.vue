d69d8fa1395a9a56f2155d90d2321be222243bbc
chatti_nader <chatti.tkd@gmail.com>
2019-01-06 01:56:49 +0100

    Ajout d'une map GoogleMap qui afiche la position de chaque restaurant

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,7 +5,7 @@
     <h1 class="page-header">A propos</h1>
     <p><b>Gestionnaire de restaurants</b></p>
     <p>Ce site contient une liste de restaurants présentés sous forme d'un tableau sur plusieurs pages. Vous pouvez visualiser les informations
-      de chaque restaurant (nom, type de cuisine, classe et adresse).</p>
+      de chaque restaurant (nom, type de cuisine, classe et adresse), ainsi que la position GPS sur une carte GoogleMap.</p>
     <p>Vouv avez la possibilité de soit modifier les informations d'un restaurant existant, de supprimer ou d'ajouter un restaurant.</p>
     <p>Pour rechercher un restaurant dans le tableau il suffit de rentrer le nom de ce dernier dans la barre de recherhce.</p>
     <hr>
